32daff2fe4f01579ac69546e9b75064050a45874
IvelianPortatilWindows <ivelian.kolev@enti.cat>
2023-11-23 19:02:49 +0100

    Intento de arreglo del codigo

--- eftp/server.sh
+++ eftp/server.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-CLIENT=ip address | grep inet | grep enp0s3 | cut -d ' ' -f6 | cut -d '/' -f1
+CLIENT=$(ip address | grep inet | grep enp0s3 | cut -d ' ' -f6 | cut -d '/' -f1)
 echo "Servidor de EFTP"
 
-echo "(0) Listen"
-DATA=`nc -l -p 3333 -w 0`
+# (0) Listen
+DATA=$(nc -l -p 3333 -w 0)
 echo $DATA
 
-echo "(3) Test & Send"
-if [ "$DATA" != "EFTP 1.0" ]
-then
-	echo "ERROR 1: BAD HEADER"
-	sleep 1
-	echo "KO_HEADER" | nc $CLIENT 3333
-	exit 1
+# (3) Test & Send
+if [ "$DATA" != "EFTP 1.0" ]; then
+    echo "ERROR 1: BAD HEADER"
+    sleep 1
+    echo "KO_HEADER" | nc $CLIENT 3333
+    exit 1
 fi
 echo "OK_HEADER"
 sleep 1
 echo "OK_HEADER" | nc $CLIENT 3333
 
-echo "(4) Listen"
-DATA=`nc -l -p 3333 -w 0`
+# (4) Listen
+DATA=$(nc -l -p 3333 -w 0)
 echo $DATA
 
-echo "(7) Test & Send"
-if [ "$DATA" != "BOOOM" ]
-then echo "ERROR 2: BAD HANDSHAKE"
-sleep 1
-echo "KO_HANDSHAKE" | nc $CLIENT 3333
-exit 2
+# (7) Test & Send
+if [ "$DATA" != "BOOOM" ]; then
+    echo "ERROR 2: BAD HANDSHAKE"
+    sleep 1
+    echo "KO_HANDSHAKE" | nc $CLIENT 3333
+    exit 2
 fi
 echo "OK_HANDSHAKE"
 sleep 1
 echo "OK_HANDSHAKE" | nc $CLIENT 3333
 
-echo "(8) Listen"
-DATA=`nc -l -p 3333 -w 0`
+# (8) Listen
+DATA=$(nc -l -p 3333 -w 0)
+echo $DATA
+
