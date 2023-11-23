2d5d3e37b7c101298d311d33bb625d1fd0e6e30e
IvelianPortatilWindows <ivelian.kolev@enti.cat>
2023-11-23 18:16:13 +0100

    De vuelta al codigo base

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
-
-# IP del servidor (localhost en este caso)
 SERVER="localhost"
-
 echo "Cliente de EFTP"
-
-# Obtención de la dirección IP del cliente
-IP_CLIENTE=$(ip address | grep inet | grep enp0s3 | awk '{print $2}' | cut -d '/' -f1)
-
 echo "(1) Send"
-# Enviar la cabecera y la IP del cliente al servidor
-echo "EFTP 1.0 $IP_CLIENTE" | nc $SERVER 3333
-
+echo "EFTP 1.0" | nc $SERVER 3333
 echo "(2) Listen"
-# Esperar respuesta del servidor
-DATA=$(nc -l -p 3333 -w 0)
-
+DATA=`nc -l -p 3333 -w -0`
 echo $DATA
-
-echo "(5) Test & Send"
-
-# Verificar la respuesta del servidor
-if [ "$DATA" != "OK_HEADER" ]; then
-    echo "ERROR 1: BAD HEADER"
-    exit 1
+echo "(5) Test $ Send"
+if [ "$DATA" != "OK_HEADER" ]
+then
+	echo "ERROR 1: BAD HEADER"
+	exit 1
 fi
-
-# Enviar confirmación al servidor
+echo "BOOOM"
+sleep 1
 echo "BOOOM" | nc $SERVER 3333
-
 echo "(6) Listen"
-# Esperar la respuesta final del servidor
-DATA=$(nc -l -p 3333 -w 0)
-
+DATA=`nc -l -p 3333 -w 0`
 echo $DATA
--- eftp/server.sh
+++ eftp/server.sh
@@ -1,47 +1,32 @@
 #!/bin/bash
-
+CLIENT="172.31.98.73"
 echo "Servidor de EFTP"
-
-# Obtención de la dirección IP del servidor (para uso en la red local)
-SERVER_IP=$(ip address | grep inet | grep enp0s3 | awk '{print $2}' | cut -d '/' -f1)
-
-echo "(1) Listen"
-# Esperar datos del cliente
-DATA=$(nc -l -p 3333 -w 0)
+echo "(0) Listen"
+DATA=`nc -l -p 3333 -w 0`
 echo $DATA
-
-# Extraer cabecera e IP del cliente
-HEADER=$(echo $DATA | cut -d ' ' -f 1-2)
-IP_CLIENTE=$(echo $DATA | cut -d ' ' -f 3)
-
 echo "(3) Test & Send"
-
-# Verificar la cabecera
-if [ "$HEADER" != "EFTP 1.0" ]; then
-    echo "ERROR 1: BAD HEADER"
-    echo "KO_HEADER" | nc $IP_CLIENTE 3333
-    exit 1
+if [ "$DATA" != "EFTP 1.0" ]
+then
+	echo "ERROR 1: BAD HEADER"
+	sleep 1
+	echo "KO_HEADER" | nc $CLIENT 3333
+	exit 1
 fi
-
-# Enviar confirmación al cliente
-echo "OK_HEADER" | nc $IP_CLIENTE 3333
-
+echo "OK_HEADER"
+sleep 1
+echo "OK_HEADER" | nc $CLIENT 3333
 echo "(4) Listen"
-# Esperar confirmación del cliente
-DATA=$(nc -l -p 3333 -w 0)
+DATA=`nc -l -p 3333 -w 0`
 echo $DATA
-
 echo "(7) Test & Send"
-# Verificar la confirmación del cliente
-if [ "$DATA" != "BOOOM" ]; then
-    echo "ERROR 2: BAD HANDSHAKE"
-    echo "KO_HANDSHAKE" | nc $IP_CLIENTE 3333
-    exit 2
+if [ "$DATA" != "BOOOM" ]
+then echo "ERROR 2: BAD HANDSHAKE"
+sleep 1
+echo "KO_HANDSHAKE" | nc $CLIENT 3333
+exit 2
 fi
-
-# Enviar la respuesta final al cliente
-echo "OK_HANDSHAKE" | nc $IP_CLIENTE 3333
-
+echo "OK_HANDSHAKE"
+sleep 1
+echo "OK_HANDSHAKE" | nc $CLIENT 3333
 echo "(8) Listen"
-# Esperar cualquier dato adicional del cliente
-DATA=$(nc -l -p 3333 -w 0)
+DATA=`nc -l -p 3333 -w 0`
